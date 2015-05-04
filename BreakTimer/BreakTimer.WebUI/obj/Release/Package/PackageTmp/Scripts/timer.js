5a12b12b772fd168c15aacd3f096a4509fc84e59
Dan <dfernandez226@gmail.com>
2015-05-04 17:04:13 -0400

    Pause, Icons
    Added pause feature to the timer. Changed buttons to icons.

--- BreakTimer/BreakTimer.WebUI/obj/Release/Package/PackageTmp/Scripts/timer.js
+++ BreakTimer/BreakTimer.WebUI/obj/Release/Package/PackageTmp/Scripts/timer.js
@@ -1,16 +1,20 @@
 ﻿$(document).ready(function () {
     $('.timer-text').text('0:00.0');
-    $('.change-task-btn').hide();
+    $('.change-task-btn').toggleClass('disabled');
+    $('.pause-timer').toggleClass('disabled');
 
-    var timerLength = 3000.0;
+    var timerLength =3000.0;
+    var ticker;
 
     $('.start-timer').click(function () {
         $('.start-timer').toggleClass('disabled');
+        $('.pause-timer').toggleClass('disabled');
         $('.task-icon').toggleClass('fa-spin');
         RunTimer(timerLength);
     });
 
     $('.change-task-btn').click(function () {
+        
         if ($('.task').text() == "Break") {
             $('.task').text("Work");
             timerLength = 3000.0;
@@ -19,8 +23,14 @@
             $('.task').text("Break");
             timerLength = 600.0;
         }
-        $('.change-task-btn').hide();
+        $('.start-timer').toggleClass('disabled');
+        $('.change-task-btn').toggleClass('disabled');
+    });
 
+    $('.pause-timer').click(function () {
+        timerLength = Number($('.timer-text').text().split(":")[0]) * 60.0 + Number($('.timer-text').text().split(":")[1]);
+        StopTimer();
+        $('.start-timer').toggleClass('disabled');
     });
 
 });
@@ -28,21 +38,21 @@
 function RunTimer(length) {
     var start = new Date().getTime();
 
-    var ticker = setInterval(function () {
+    ticker = setInterval(function () {
         var time = new Date().getTime() - start;
-        elapsed = Math.floor(time / 100) / 10;
-        var remaining = Math.round((length - elapsed) * 10) / 10;
-        $('.timer-text').text(FormatTime(remaining));
+        var elapsed = Math.floor(time / 100) / 10;
+        var secondsRemaining = Math.round((length - elapsed) * 10) / 10;
+        $('.timer-text').text(FormatTime(secondsRemaining));
 
-        
-        if (remaining <= 0)
+        if (secondsRemaining <= 0)
         {
-            StopTimer(ticker);
+            StopTimer();
             window.alert($('.task').text() + " over!");
-            
+            $('.change-task-btn').toggleClass('disabled');
         }
     }, 100);
 }
+
 
 function FormatTime(totalSeconds) {
     var minutes = Math.floor(totalSeconds / 60.0);
@@ -52,10 +62,9 @@ function FormatTime(totalSeconds) {
     return minutes.toString() + ':' + seconds.toString();
 }
 
-function StopTimer(interval) {
-    clearInterval(interval);
-    $('.start-timer').toggleClass('disabled');
+function StopTimer() {
+    clearInterval(ticker);
+    $('.pause-timer').toggleClass('disabled');
     $('.task-icon').toggleClass('fa-spin');
-    $('.change-task-btn').show();
 }
 
