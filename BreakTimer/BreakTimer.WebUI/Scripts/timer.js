b60f764d10f8bfe866bbea3c4aa8cf0a1a848d03
Dan <dfernandez226@gmail.com>
2015-05-06 15:06:08 -0400

    Custom Times
    Changed timer display to a text input to allow for custom time lengths
    to be set.

--- BreakTimer/BreakTimer.WebUI/Scripts/timer.js
+++ BreakTimer/BreakTimer.WebUI/Scripts/timer.js
@@ -1,48 +1,56 @@
 ﻿$(document).ready(function () {
-    $('.timer-text').text('0:00.0');
+    SetTimer('50', '0');
     $('.change-task-btn').toggleClass('disabled');
     $('.pause-timer').toggleClass('disabled');
 
-    var timerLength =3000.0;
+    var timerLength = 3000.0;
     var ticker;
 
-    $('.start-timer').click(function () {
+    $('.start-timer').click(function ()
+    {
         $('.start-timer').toggleClass('disabled');
         $('.pause-timer').toggleClass('disabled');
         $('.task-icon').toggleClass('fa-spin');
+        timerLength = Number($('.minutes').val()) * 60.0 + Number($('.seconds').val());
         RunTimer(timerLength);
     });
 
-    $('.change-task-btn').click(function () {
-        
-        if ($('.task').text() == "Break") {
+    $('.change-task-btn').click(function ()
+    {
+        if ($('.task').text() == "Break")
+        {
             $('.task').text("Work");
-            timerLength = 3000.0;
+            SetTimer('50', '0');
         }
-        else {
+        else
+        {
             $('.task').text("Break");
-            timerLength = 600.0;
+            SetTimer('10', '0');
         }
+
         $('.start-timer').toggleClass('disabled');
         $('.change-task-btn').toggleClass('disabled');
     });
 
-    $('.pause-timer').click(function () {
-        timerLength = Number($('.timer-text').text().split(":")[0]) * 60.0 + Number($('.timer-text').text().split(":")[1]);
+    $('.pause-timer').click(function ()
+    {
         StopTimer();
         $('.start-timer').toggleClass('disabled');
     });
 
 });
 
-function RunTimer(length) {
+function RunTimer(length)
+{
     var start = new Date().getTime();
 
-    ticker = setInterval(function () {
+    ticker = setInterval(function ()
+    {
         var time = new Date().getTime() - start;
         var elapsed = Math.floor(time / 100) / 10;
         var secondsRemaining = Math.round((length - elapsed) * 10) / 10;
-        $('.timer-text').text(FormatTime(secondsRemaining));
+        $('.minutes').val(FormatMinutes(secondsRemaining));
+        $('.seconds').val(FormatSeconds(secondsRemaining));
 
         if (secondsRemaining <= 0)
         {
@@ -54,17 +62,32 @@ function RunTimer(length) {
 }
 
 
-function FormatTime(totalSeconds) {
-    var minutes = Math.floor(totalSeconds / 60.0);
+function FormatMinutes(totalSeconds)
+{
+    return Math.floor(totalSeconds / 60.0).toString();
+}
+
+function FormatSeconds(totalSeconds)
+{
     var seconds = Math.round((totalSeconds % 60.0) * 10) / 10;
     if (seconds < 10) { seconds = '0' + seconds; }
     if (Math.round(seconds) == seconds) { seconds += '.0'; }
-    return minutes.toString() + ':' + seconds.toString();
+    return seconds.toString();
 }
 
-function StopTimer() {
+function StopTimer()
+{
     clearInterval(ticker);
     $('.pause-timer').toggleClass('disabled');
     $('.task-icon').toggleClass('fa-spin');
 }
 
+function SetTimer(minutes, seconds)
+{
+    $('.minutes').val(minutes);
+    $('.seconds').val(seconds);
+}
+
+function playSound(filename) {
+    document.getElementById("sound").innerHTML = '<audio autoplay="autoplay"><source src="~/Sounds/pop.mp3" type="audio/mpeg" /></audio>';
+}
